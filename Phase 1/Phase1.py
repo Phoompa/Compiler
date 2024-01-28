eb07f01d224ddc35ed3dd8dccdebdc5aa642af12
Ajay Sodhi <ajay_sodhi@outlook.com>
2024-01-28 02:15:35 -0500

    Everything working fine, commit before making lexical analysis its own function

--- Phase 1/Phase1.py
+++ Phase 1/Phase1.py
@@ -1,5 +1,5 @@
 import csv
-BUFFER_SIZE = 50
+BUFFER_SIZE = 1024
 START = 0
 
 def csv_to_2Darray(file_path): #function to convert the transition table .csv file into a 2D array
@@ -79,6 +79,9 @@ def getToken(state,file): #function used to obtain token given a state
     file.write("\n")
 
 
+#def lexical_analysis(transition_table, keywords, final_states, decrement_final_states):
+
+
 
 transition_table = csv_to_2Darray('CP471 -- Transition Table.csv') #Initialize transition table
 keywords = keywords_to_list('keywords.txt') #initialize list of keywords
@@ -87,35 +90,46 @@ decrement_final_states = [4,7,10,21,25,29,31,36,40,45,48,50,54,60,67,72,78,80,83
 
 
 output_file = open('output.txt','w')
+error_file = open('errors.txt', 'w')
 with open('Test1.cp', 'r') as file:
     buffer1 = file.read(BUFFER_SIZE)
     buffer2 = file.read(BUFFER_SIZE)
-
+    print("Length of Buffer1")
+    print(len(buffer1))
+    print("Length of Buffer2")
+    print(len(buffer2))
     while buffer1:
         current = transition_table[0][START]    #current state
-        token_buffer = "" 
-        for char in buffer1:   #iterates through every char in buffer
+        i = 0
+        line_count = 1
+        while i < len(buffer1):   #iterates through every char in buffer
             # Process character in buffer1
             #print(int(transition_table[ord(char)][current]))
-            token_buffer=""
-            print(char)
+            print(buffer1[i])
             previous = current
-            current = int(transition_table[int(ord(char) - 1)][int(current)])
+            current = int(transition_table[int(ord(buffer1[i]) - 1)][int(current)])
             print(current)
-            token_buffer += char
             if current in final_states: #Final state reached
-                #print("final state reached: ")
-                #print(current)
+                print("final state reached: ")
                 getToken(current,output_file)
 
-                if current in decrement_final_states:
+                if current in decrement_final_states: #decrement one char back
                     print("FINAL STATE REACHED")
-                    print(char)
-                    file.seek(file.tell() - len(token_buffer))
+                    print(buffer1[i])
+                    i = i - 1
 
                     
                 current = transition_table[0][START]
-                token_buffer = ""
+            elif current == -1: #Error
+                print("ERROR")
+                error_file.write("Char: " +  buffer1[i] + " line #: " + str(line_count))
+                current = transition_table[0][START]
+
+            if (ord(buffer1[i]) == 10): # /n encountered
+                line_count += 1
+
+
+            i+= 1
                 
             
 
@@ -125,10 +139,9 @@ with open('Test1.cp', 'r') as file:
         print("-------------------------------------------BUFFER SWITCH-------------------------------------------")
         buffer2 = file.read(BUFFER_SIZE)
 
-# Process any remaining characters in the last buffer
-for char in buffer2:
-    print('h')
+
 print(transition_table[36][0])
 print(final_states)
 output_file.close()
+error_file.close()
 print(len(buffer1))
